beca46e29c48ad3dab6ace4122f7079d41fe3d09
NBCorrigan <nickb.corrigan@gmail.com>
2018-12-04 15:00:24 -0800

    small refactor of list api route

--- routes/listApiRoutes.js
+++ routes/listApiRoutes.js
@@ -2,20 +2,20 @@ var models = require("../models");
 var passport = require("passport");
 
 
-module.exports = function(app,passport) {
+module.exports = function(app) {
 
 
- // GET route for getting all of the todos
- app.get("/api/list", function(req, res) {
+ // GET route for getting all of the lists
+ app.get("/api/lists", function(req, res) {
   // findAll returns all entries for a table when used with no options
      models.List.findAll({}).then(function(modelsList) {
-    // We have access to the todos as an argument inside of the callback function
+    // We have access to the lists as an argument inside of the callback function
     res.json(modelsList);
   });
 });
 
-// POST route for saving a new todo
-app.post("/api/list", function(req, res) {
+// POST route for saving a new list
+app.post("/api/lists", function(req, res) {
   // create takes an argument of an object describing the item we want to
   // insert into our table. In this case we just we pass in an object with a text
   // and complete property (req.body)
@@ -23,7 +23,7 @@ models.List.create({
     text: req.body.text,
     complete: req.body.complete
   }).then(function(modelsList) {
-    // We have access to the new todo as an argument inside of the callback function
+    // We have access to the new list as an argument inside of the callback function
     res.json(modelsList);
   })
     .catch(function(err) {
@@ -33,10 +33,10 @@ models.List.create({
     });
 });
 
-// DELETE route for deleting todos. We can get the id of the todo to be deleted from
+// DELETE route for deleting lists. We can get the id of the list to be deleted from
 // req.params.id
-app.delete("/api/list/:id", function(req, res) {
-  // We just have to specify which todo we want to destroy with "where"
+app.delete("/api/lists/:id", function(req, res) {
+  // We just have to specify which list we want to destroy with "where"
 models.List.destroy({
     where: {
       id: req.params.id
@@ -47,8 +47,8 @@ models.List.destroy({
 
 });
 
-// PUT route for updating todos. We can get the updated todo data from req.body
-app.put("/api/list", function(req, res) {
+// PUT route for updating lists. We can get the updated list data from req.body
+app.put("/api/lists", function(req, res) {
 
   // Update takes in an object describing the properties we want to update, and
   // we use where to describe which objects we want to update
