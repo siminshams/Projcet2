c3a9d35a996d03578b9d42af3886aa752bbd35f0
Bryan Magers <bmagers@users.noreply.github.com>
2018-12-04 14:51:04 -0800

    movie details api call

--- routes/TMDbApiRoutes.js
+++ routes/TMDbApiRoutes.js
@@ -3,14 +3,17 @@ require("dotenv").config();
 var env = require("dotenv").load();
 var keys = require("../config/keys");
 
-module.exports = function(app) {
+// function simplifies URL for calls to The Movie Database API
+var TMDbUrl = function(query, additionalParameters) {
   var api_key = keys.TMDb.api_key;
   var baseUrl = "https://api.themoviedb.org/3/";
   var baseParameters = "?language=en-US&include_video=false&include_adult=false&api_key=" + api_key;
-  var TMDbUrl = function(query, additionalParameters) {
-    return baseUrl + query + baseParameters + additionalParameters;
-  }
+  return baseUrl + query + baseParameters + additionalParameters;
+}
 
+module.exports = function(app) {
+
+  // popular current movies
   app.get("/", function(req, res) {
     var url = TMDbUrl("discover/movie", "&page=1&sort_by=popularity.desc");
     request(url, function(error, result, body) {
@@ -28,6 +31,7 @@ module.exports = function(app) {
     });
   });
 
+  // search movies
   app.post("/api/search", function(req, res) {
     var url = TMDbUrl("search/movie", "&query=" + encodeURI(req.body.query));
     request(url, function(error, result, body) {
@@ -35,6 +39,16 @@ module.exports = function(app) {
       var response = JSON.parse(body);
       var searchResults = response.results;
       res.json(searchResults);
+    });
+  });
+
+  // movie details
+  app.get("/api/movie/:movie", function(req, res) {
+    var url = TMDbUrl("movie/" + req.params.movie, "");
+    request(url, function(error, result, body) {
+      if (error) { return console.log(error); }
+      var response = JSON.parse(body);
+      res.json(response);
     });
   });
 
