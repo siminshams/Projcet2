5245722d6b1ce5580b3d7e71cafcb6649c8c610a
Bryan Magers <bmagers@users.noreply.github.com>
2018-12-04 13:30:12 -0800

    additional minor refactor in TMDbApiRoutes

--- routes/TMDbApiRoutes.js
+++ routes/TMDbApiRoutes.js
@@ -7,13 +7,13 @@ module.exports = function(app) {
   var api_key = keys.TMDb.api_key;
   var baseUrl = "https://api.themoviedb.org/3/";
   var baseParameters = "?language=en-US&include_video=false&include_adult=false&api_key=" + api_key;
-  var TMDbUrl = function(query) {
-    var result = baseUrl + query + baseParameters;
+  var TMDbUrl = function(query, additionalParameters) {
+    var result = baseUrl + query + baseParameters + additionalParameters;
     return result;
   }
 
   app.get("/", function(req, res) {
-    var url = TMDbUrl("discover/movie") + "&page=1&sort_by=popularity.desc";
+    var url = TMDbUrl("discover/movie", "&page=1&sort_by=popularity.desc");
     request(url, function(error, result, body) {
       if (error) { return console.log(error); }
       var response = JSON.parse(body);
@@ -30,7 +30,7 @@ module.exports = function(app) {
   });
 
   app.post("/api/search", function(req, res) {
-    var url = TMDbUrl("search/movie") + "&query=" + encodeURI(req.body.query);
+    var url = TMDbUrl("search/movie", "&query=" + encodeURI(req.body.query));
     request(url, function(error, result, body) {
       if (error) { return console.log(error); }
       var response = JSON.parse(body);
