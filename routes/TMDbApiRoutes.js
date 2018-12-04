74514a03be2cd5584e4d7fad9c02746287d73a9a
Bryan Magers <bmagers@users.noreply.github.com>
2018-12-04 13:02:08 -0800

    search api route

--- routes/TMDbApiRoutes.js
+++ routes/TMDbApiRoutes.js
@@ -4,11 +4,13 @@ var env = require("dotenv").load();
 var keys = require("../config/keys");
 
 module.exports = function(app) {
+  var api_key = keys.TMDb.api_key;
+  var baseUrl = "https://api.themoviedb.org/3/";
+  var baseParameters = "?language=en-US&include_video=false&include_adult=false&api_key=" + api_key;
 
   app.get("/", function(req, res) {
-    var api_key = keys.TMDb.api_key;
-    var url = "https://api.themoviedb.org/3/discover/movie?page=1&include_video=false&include_adult=false&sort_by=popularity.desc&language=en-US&api_key=" + api_key;
-
+    var url = baseUrl + "discover/movie" + baseParameters;
+    url += "&page=1&sort_by=popularity.desc";
     request(url, function(error, result, body) {
       if (error) { return console.log(error); }
       var response = JSON.parse(body);
@@ -22,7 +24,17 @@ module.exports = function(app) {
         posters: posters
       });
     });
+  });
 
+  app.post("/api/search", function(req, res) {
+    var url = baseUrl + "search/movie" + baseParameters;
+    url += "&query=" + encodeURI(req.body.query);
+    request(url, function(error, result, body) {
+      if (error) { return console.log(error); }
+      var response = JSON.parse(body);
+      var searchResults = response.results;
+      res.json(searchResults);
+    });
   });
 
 }
