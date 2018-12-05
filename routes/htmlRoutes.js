dc361f9863a3b7b47a1babb29508f7d80694b8d1
Bryan Magers <bmagers@users.noreply.github.com>
2018-12-05 13:58:09 -0800

    authentication routes and unimproved UI

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,27 +1,17 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Load index page
-  app.get("/", function(req, res) {
-      db.user.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
+
+  app.get("/signin", function(req, res) {
+    res.render("signin");
   });
 
-  // Load example page and pass in an example by id
-  app.get("/signup/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
+  app.get("/signup", function(req, res) {
+    res.render("signup");
   });
 
-  // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
     res.render("404");
   });
+
 };
