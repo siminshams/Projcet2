1856ae3b72489fe1c1cebe62b829289856e10054
NBCorrigan <nickb.corrigan@gmail.com>
2018-12-04 20:11:09 -0800

    refactored main.handlebars and updated index.handlebars layout

--- public/js/view.js
+++ public/js/view.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
-  // Getting a reference to the input field where user adds a new todo
+
   var $newItemInput = $("input.new-item");
-  // Our new todos will go inside the todoContainer
+
   var $listContainer = $(".list-container");
-  // Adding event listeners for deleting, editing, and adding todos
+
   $(document).on("click", "button.delete", deleteList);
   $(document).on("click", "button.complete", toggleComplete);
   $(document).on("click", ".list-item", editList);
@@ -11,13 +11,13 @@ $(document).ready(function() {
   $(document).on("blur", ".list-item", cancelEdit);
   $(document).on("submit", "#list-form", insertList);
 
-  // Our initial todos array
+  
   var lists = [];
 
-  // Getting todos from database when page loads
+  
   getLists();
 
-  // This function resets the todos displayed with new todos from the database
+  
   function initializeRows() {
     $listContainer.empty();
     var rowsToAdd = [];
@@ -27,7 +27,7 @@ $(document).ready(function() {
     $listContainer.prepend(rowsToAdd);
   }
 
-  // This function grabs todos from the database and updates the view
+ 
   function getLists() {
     $.get("/api/lists", function(data) {
       lists = data;
@@ -35,7 +35,7 @@ $(document).ready(function() {
     });
   }
 
-  // This function deletes a todo when the user clicks the delete button
+  
   function deleteList(event) {
     event.stopPropagation();
     var id = $(this).data("id");
@@ -45,7 +45,7 @@ $(document).ready(function() {
     }).then(getLists);
   }
 
-  // This function handles showing the input box for a user to edit a todo
+
   function editList() {
     var currentList = $(this).data("list");
     $(this).children().hide();
@@ -54,16 +54,15 @@ $(document).ready(function() {
     $(this).children("input.edit").focus();
   }
 
-  // Toggles complete status
+
   function toggleComplete(event) {
     event.stopPropagation();
-    var todo = $(this).parent().data("list");
+    var list = $(this).parent().data("list");
     list.complete = !list.complete;
     updateList(list);
   }
 
-  // This function starts updating a todo in the database if a user hits the "Enter Key"
-  // While in edit mode
+
   function finishEdit(event) {
     var updatedList = $(this).data("list");
     if (event.which === 13) {
@@ -73,7 +72,7 @@ $(document).ready(function() {
     }
   }
 
-  // This function updates a todo in our database
+  
   function updateList(list) {
     $.ajax({
       method: "PUT",
@@ -82,8 +81,7 @@ $(document).ready(function() {
     }).then(getLists);
   }
 
-  // This function is called whenever a todo item is in edit mode and loses focus
-  // This cancels any edits being made
+
   function cancelEdit() {
     var currentList = $(this).data("list");
     if (currentList) {
@@ -94,11 +92,11 @@ $(document).ready(function() {
     }
   }
 
-  // This function constructs a todo-item row
+
   function createNewRow(list) {
     var $newInputRow = $(
       [
-        "<li class='list-group-item todo-item'>",
+        "<li class='list-group-item list-item'>",
         "<span>",
         list.text,
         "</span>",
@@ -118,9 +116,10 @@ $(document).ready(function() {
     return $newInputRow;
   }
 
-  // This function inserts a new todo into our database and then updates the view
+  
   function insertList(event) {
     event.preventDefault();
+    console.log('insertList');
     var list = {
       text: $newItemInput.val().trim(),
       complete: false
--- routes/listApiRoutes.js
+++ routes/listApiRoutes.js
@@ -1,42 +1,37 @@
 var models = require("../models");
-var passport = require("passport");
+// var passport = require("passport");
 
 
 module.exports = function(app) {
 
 
- // GET route for getting all of the lists
+
  app.get("/api/lists", function(req, res) {
-  // findAll returns all entries for a table when used with no options
+
      models.List.findAll({}).then(function(modelsList) {
-    // We have access to the lists as an argument inside of the callback function
+
     res.json(modelsList);
   });
 });
 
-// POST route for saving a new list
+
 app.post("/api/lists", function(req, res) {
-  // create takes an argument of an object describing the item we want to
-  // insert into our table. In this case we just we pass in an object with a text
-  // and complete property (req.body)
+
 models.List.create({
     text: req.body.text,
     complete: req.body.complete
   }).then(function(modelsList) {
-    // We have access to the new list as an argument inside of the callback function
+   
     res.json(modelsList);
   })
     .catch(function(err) {
-    // Whenever a validation or flag fails, an error is thrown
-    // We can "catch" the error to prevent it from being "thrown", which could crash our node app
+
       res.json(err);
     });
 });
 
-// DELETE route for deleting lists. We can get the id of the list to be deleted from
-// req.params.id
 app.delete("/api/lists/:id", function(req, res) {
-  // We just have to specify which list we want to destroy with "where"
+  
 models.List.destroy({
     where: {
       id: req.params.id
@@ -47,11 +42,9 @@ models.List.destroy({
 
 });
 
-// PUT route for updating lists. We can get the updated list data from req.body
 app.put("/api/lists", function(req, res) {
 
-  // Update takes in an object describing the properties we want to update, and
-  // we use where to describe which objects we want to update
+
 models.List.update({
     text: req.body.text,
     complete: req.body.complete
@@ -63,8 +56,7 @@ models.List.update({
     res.json(modelsList);
   })
     .catch(function(err) {
-    // Whenever a validation or flag fails, an error is thrown
-    // We can "catch" the error to prevent it from being "thrown", which could crash our node app
+
       res.json(err);
     });
 });
