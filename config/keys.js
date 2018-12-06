a23ced075939b54aa0ddbc7c13610c1e35e6b426
Simin Shamsfallah <Simin.Shamsfallah@Earlens.com>
2018-12-05 16:18:36 -0800

    Added NewsApi, updated user database, added serialize

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,10 @@
 console.log("Keys loaded");
+var exports = module.exports = {}
 
 exports.TMDb = {
   api_key: process.env.TMDB_APIKEY
+};
+
+exports.NEWS = {
+  api_key: process.env.NEWS_APIKEY
 };
--- models/user.js
+++ models/user.js
@@ -1,18 +1,5 @@
 module.exports = function(sequelize, DataTypes) { 
-  var User = sequelize.define('user', {
-      id: {
-          autoIncrement: true,
-          primaryKey: true,
-          type: DataTypes.INTEGER
-      },
-      firstname: {
-          type: DataTypes.STRING,
-          notEmpty: true
-      },
-      lastname: {
-          type: DataTypes.STRING,
-          notEmpty: true
-      },
+  var User = sequelize.define('user', {    
       username: {
           type: DataTypes.TEXT
       },
@@ -25,14 +12,7 @@ module.exports = function(sequelize, DataTypes) {
       password: {
           type: DataTypes.STRING,
           allowNull: false
-      },
-      last_login: {
-          type: DataTypes.DATE
-      },
-      status: {
-          type: DataTypes.ENUM('active', 'inactive'),
-          defaultValue: 'active'
-      }
+      }      
   });
   return User;
 
